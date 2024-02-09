2e7bb6568761f7bab33fead8036db8cc6e7253b8
massygh <massyghen1@gmail.com>
2024-02-09 15:38:32 -0800

    test

--- app.py
+++ app.py
@@ -1,33 +1,20 @@
 # app.py
-import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Exemple de structure de données pour les joueurs
 joueurs = [
-    {"id": 1, "nom": "Messi", "equipe": "Miami FC"},
-    {"id": 2, "nom": "Ronaldo", "equipe": "Al Nassr"},
-    {"id": 3, "nom": "Wail", "equipe": "Paris Saint-Germain"},
-    # Ajoutez d'autres joueurs
-]
-
-# Exemple de structure de données pour les équipes
-equipes = [
-    {"id": 1, "nom": "Miami FC"},
-    {"id": 2, "nom": "Al Nassr"},
-    {"id": 3, "nom": "Paris Saint-Germain"},
-    # Ajoutez d'autres équipes
+    {"id": 1, "nom": "Messi", "equipe": "Miami FC", "stats": {"attaque": 90, "defense": 70, "physique": 85}},
+    {"id": 2, "nom": "Ronaldo", "equipe": "Al Nassr", "stats": {"attaque": 92, "defense": 65, "physique": 88}},
+    {"id": 3, "nom": "Wail", "equipe": "Paris Saint-Germain", "stats": {"attaque": 85, "defense": 75, "physique": 80}},
+    # Ajoutez d'autres joueurs avec leurs statistiques
 ]
 
 @app.route('/')
 def accueil():
-    return render_template('accueil.html', joueurs=joueurs, equipes=equipes)
-
-@app.route('/page-inaccessible')
-def page_inaccessible():
-    message = "Cette page est inaccessible pour le moment."
-    return render_template('page_inaccessible.html', message=message)
+    return render_template('accueil.html', joueurs=joueurs)
 
 if __name__ == '__main__':
     random_port = random.randint(5000, 9999)
